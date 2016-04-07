c1b0cc0b23763193c06d19f1f51f062fa1b7a35a
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-04-07 15:51:49 -0700

    [feat] work on  from expedia

--- www/js/addEventCtrl.js
+++ www/js/addEventCtrl.js
@@ -1,11 +1,18 @@
 angular.module('jauntly.addEventCtrl', [])
 
-.controller('addEventCtrl', function($scope, $http) {
-  $scope.url = "http://terminal2.expedia.com/x/activities/search?location=London&apikey=" + expKey;
-  $http.get($scope.url)
-  .then(function(res){
-    $scope.obj = res.data;
-    console.log(res.data);
-  })
+.controller('addEventCtrl', function($scope, ExpediaInfo) {
+  $scope.results = {};
+
+  $scope.search = function (location, activity) {
+    ExpediaInfo.getExpInfo(location, activity)
+    .then(function (results){
+      $scope.results = results.data.activities;
+      console.log('ajax get results ', $scope.results);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  };
+
 
 });
---
+++ www/js/services.js
@@ -0,0 +1,20 @@
+angular.module('jauntly.services', [])
+
+.factory('ExpediaInfo', function ($http) {
+  var getExpInfo = function (location, activity) {
+    return $http({
+      method      : 'GET',
+      contentType : 'application/json',
+      params      : {
+        location   : location,
+        activity   : activity,
+        apikey     : expKey
+      },
+      url         : 'http://terminal2.expedia.com/x/activities/search'
+    })
+  };
+
+  return {
+    getExpInfo : getExpInfo
+  };
+});
