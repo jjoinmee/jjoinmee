30e949fb0856e6e6bc03acc83113f79f8b82c654
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-04-07 20:03:34 -0700

    [feat] google geolocation

--- www/js/services.js
+++ www/js/services.js
@@ -14,8 +14,25 @@ angular.module('jauntly.services', [])
       url         : 'http://terminal2.expedia.com/x/activities/search'
     })
   };
-
   return {
     getExpInfo : getExpInfo
+  };
+})
+
+
+.factory('GoogleGeocodeInfo', function ($http) {
+  var getAddress = function (latlng) {
+    return $http({
+      method      : 'GET',
+      contentType : 'application/json',
+      params      : {
+        latlng : latlng,
+        key    : googleMapsApiKey
+      },
+      url         : 'https://maps.googleapis.com/maps/api/geocode/json'
+    })
+  };
+  return {
+    getAddress : getAddress
   };
 });
