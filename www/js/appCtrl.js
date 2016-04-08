e804fd7c321a6c911d3652d8dec0114a963c689b
Jane Fong <janeefongg@gmail.com>
2016-04-08 15:33:17 -0700

    [feat] integrate facebook oauth

--- www/js/appCtrl.js
+++ www/js/appCtrl.js
@@ -1,6 +1,17 @@
 angular.module('jauntly.appCtrl', [])
 
-.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout, ngFB) {
+  $scope.fbLogin = function () {
+    ngFB.login({scope: 'email,read_stream,publish_actions'}).then(
+      function (response) {
+        if (response.status === 'connected') {
+          console.log('Facebook login succeeded');
+          $scope.closeLogin();
+        } else {
+          alert('Facebook login failed');
+        }
+      });
+  };
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
