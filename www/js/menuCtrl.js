172329964f6884a7269f88cb813d95048441e7ff
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-04-11 10:51:37 -0700

    [feat] integrate login part of oauth

---
+++ www/js/menuCtrl.js
@@ -0,0 +1,12 @@
+angular.module('jauntly.menuCtrl', [])
+
+.controller('menuCtrl', function($scope, Auth) {
+  $scope.isLoggedIn = function(){
+    if (Auth.authData.facebook.accessToken) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+})
